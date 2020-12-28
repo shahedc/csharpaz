b640f1e335b494cbb0bfd502033e4819cadde476
shahedc <shahedc@gmail.com>
2020-12-28 00:10:45 -0500

    added 2 projects for A-Z prelude

---
+++ src/azprelude-net5/Program.cs
@@ -0,0 +1,1 @@
+﻿System.Console.WriteLine("Hello World from .NET 5 using C#9!");
---
+++ src/azprelude/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azprelude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from .NET Core 3.1!");
+        }
+    }
+}
