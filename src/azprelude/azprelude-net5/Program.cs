02c7c20469af67d880b3c93e7c3223108a145828
shahedc <shahedc@gmail.com>
2020-12-28 01:36:16 -0500

    Added command line args

--- src/azprelude/azprelude-net5/Program.cs
+++ src/azprelude/azprelude-net5/Program.cs
@@ -1,1 +1,12 @@
-﻿System.Console.WriteLine("Hello World from .NET 5 using C#9!");
+﻿using System;
+
+Console.WriteLine("Hello World from .NET 5 using C#9!");
+
+var age = args[0];
+var title = args[1];
+var idnumber = args[2];
+
+Console.WriteLine($"The age is {age}.");
+Console.WriteLine($"The title is {title}.");
+Console.WriteLine($"The idnumber is {idnumber}.");
+
