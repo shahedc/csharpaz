183d7a386afd556f4b050c5dd7f9955ce14667bb
shahedc <shahedc@gmail.com>
2021-01-16 14:07:59 -0500

    initial projects for bits in native integers

---
+++ src/az-bit-nativeint/bit-netcore31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bit_netcore31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET Core 3.1!");
+        }
+    }
+}
---
+++ src/az-bit-nativeint/bit-netcore5/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+
+Console.WriteLine("Hello from .NET 5!");
