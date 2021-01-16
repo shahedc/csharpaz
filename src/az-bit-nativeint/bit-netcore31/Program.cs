4beed52cf5ffa219d6f6e29c109b123e548489fc
shahedc <shahedc@gmail.com>
2021-01-16 14:36:00 -0500

    added weapon storage code with int bits in 3.1 program + added shared library for int constants

--- src/az-bit-nativeint/bit-netcore31/Program.cs
+++ src/az-bit-nativeint/bit-netcore31/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bitLib;
+using System;
 
 namespace bit_netcore31
 {
@@ -7,6 +8,45 @@ namespace bit_netcore31
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from .NET Core 3.1!");
+
+            // show weapons defined
+            Console.WriteLine("Weapons defined: knife, pistol, sword, stick, shotgun");
+
+            // create weapon bag
+            int myWeaponBag;
+            Console.WriteLine("Initiating weapon bag...");
+
+            // store weapons in bag
+            myWeaponBag = StoreWeaponsInBag();
+
+            // check for weapons
+            LookForWeapons(myWeaponBag);
+
+        }
+
+        private static void LookForWeapons(int myWeaponBag)
+        {
+            Console.WriteLine("Looking for weapons...");
+            ShowWeaponFound(myWeaponBag, Weapons.Knife, nameof(Weapons.Knife));
+            ShowWeaponFound(myWeaponBag, Weapons.Pistol, nameof(Weapons.Pistol));
+            ShowWeaponFound(myWeaponBag, Weapons.Sword, nameof(Weapons.Sword));
+            ShowWeaponFound(myWeaponBag, Weapons.Stick, nameof(Weapons.Stick));
+            ShowWeaponFound(myWeaponBag, Weapons.Shotgun, nameof(Weapons.Shotgun));
+
+        }
+
+        private static void ShowWeaponFound(int weaponBag, int weapon, string weaponName)
+        {            
+            Console.WriteLine((weaponBag & weapon) > 0 
+                ? $"--> Found {weaponName}" : $"NO {weaponName}");
+        }
+
+        private static int StoreWeaponsInBag()
+        {
+            int myWeaponBag;
+            Console.WriteLine("Storing a few weapons in bag (knife, pistol, stick)...");
+            myWeaponBag = Weapons.Knife | Weapons.Pistol | Weapons.Stick;
+            return myWeaponBag;
         }
     }
 }
---
+++ src/az-bit-nativeint/bitLib/Weapons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bitLib
+{
+    public static class Weapons
+    {
+        // define weapons
+        public const int Knife = 1;
+        public const int Pistol = 2;
+        public const int Sword = 4;
+        public const int Stick = 8;
+        public const int Shotgun = 16;
+
+    }
+}
